d0198a71351b7923723453d945281e7460779dc6
Emmanuel-Madrid <chris_payan@icloud.com>
2024-03-09 11:54:16 -0700

    Examen usando Api Firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Create from './components/Create';
+import Edit from './components/Edit';
+import FirebaseImageUpload from './components/FirebaseImageUpload';
+import Show from './components/Show';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Show />} />
+        <Route path='/Create' element={<Create />} />
+        <Route path='/Edit/:id' element={<Edit />} />
+        <Route path='/FirebaseImageUpload' element={<FirebaseImageUpload />} />
+      </Routes>
+      </BrowserRouter>
+
+
     </div>
   );
 }
